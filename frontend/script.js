1d131e6aafd55c2f5ce068d06b0d411be8b1ce73
Mariam Lawal <lawalmariamyetunde@gmail.com>
2022-03-08 13:30:47 +0100

    Updated the Script

--- frontend/script.js
+++ frontend/script.js
@@ -1,29 +1,41 @@
-let submitButton = document.querySelector('#submit');
-let form = document.getElementById('form');
-let nameInput = form.name.value;
-let emailInput = form.email.value;
-let phoneInput = document.querySelector('#phone').value;
-let messageInput = document.querySelector('#message').value;
+let submitButton = document.querySelector("#submit");
+let form = document.getElementById("form");
+let nameInput = form.name;
+let emailInput = form.email;
+let phoneInput = document.querySelector("#phone");
+let messageInput = document.querySelector("#message");
+let cancelButton = document.querySelector("#cancel");
 
 //Dont know why values of inputs submitted are not showing here
-form.addEventListener('submit', (e) => {
+cancelButton.addEventListener("click", (e) => {
     e.preventDefault();
+    console.log("Cancelled");
+    nameInput.value = "";
+    emailInput.value = "";
+    phoneInput.value = "";
+    messageInput.value = "";
+});
 
-    if (nameInput.length === 0 || emailInput.length === 0 || phoneInput.length === 0 || messageInput.length === 0) {
-        window.alert('all input boxes must be filled');
-        console.log(emailInput, 'Value');
-        console.log(emailInput.length, 'length');
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (
+        nameInput.value.length === 0 ||
+        emailInput.value.length === 0 ||
+        phoneInput.value.length === 0 ||
+        messageInput.value.length === 0
+    ) {
+        window.alert("all input boxes must be filled");
         return;
     }
 
-    if (!phoneInput.match(/[0-9]/)) {
-        window.alert('phone number field must only contain numbers');
+    if (isNaN(phoneInput.value)) {
+        window.alert("phone number field must only contain numbers");
         return;
     }
-    if (messageInput.length > 100) {
-        window.alert('message field cannot have more than 100 characters');
+    if (messageInput.value.length > 100) {
+        window.alert("message field cannot have more than 100 characters");
         return;
     }
 
-    window.alert('welcome ' + nameInput);
+    window.alert("welcome " + nameInput.value);
 });
